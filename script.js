afd8ae222ef25a29a5f3dea39cf568bf82509970
arthurpph <arthurpiana13@gmail.com>
2023-05-30 08:51:16 -0300

    1

---
+++ script.js
@@ -0,0 +1,7 @@
+function cadastrar() {
+    const nomecompleto = document.getElementById("nomecompleto").value
+    const telefone = document.getElementById("telefone").value
+    const cpf = document.getElementById("cpf").value
+    const email = document.getElementById("email").value
+    const senha = document.getElementById("senha").value
+}
