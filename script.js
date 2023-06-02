a67b3640f99ebafefdf3455d2991963425ae4ca0
mritaritter <mariaritaritter@gmail.com>
2023-06-02 10:43:33 -0300

    update

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ function cadastrar() {
     const cpf = document.getElementById("cpf").value
     const email = document.getElementById("email").value
     const senha = document.getElementById("senha").value
+    var res = document.getElementById("res")
+    res.innerHTML = `Seu nome: ${nomecompleto} Seu telefone: ${telefone} \nSeu CPF: ${cpf} \nSeu e-mail: ${email} \nSua senha: ${senha}`
 }
